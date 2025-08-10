ec7ff0d50186267d51045bcbe4078d2eb8efcfa8
dora-explora <sarrafdara@gmail.com>
2025-08-10 15:08:34 -0700

    commit message

--- assets/shader.fs
+++ assets/shader.fs
@@ -4,21 +4,22 @@ precision mediump float;
 // varying vec2 fragTexCoord;
 varying vec4 fragColor;
 
-vec4 color = vec4(0);
 // uniform vec2 u_resolution;
-// uniform float time;
+uniform float time;
 
-// float random(in float seed) {
-//     return fract(sin(seed) * 43758.5453123);
-// }
+vec4 color = vec4(0);
+
+float random(in float seed) {
+    return fract(sin(seed) * 43758.5453123);
+}
 
 void main()
 {
     if (fragColor.r > 0.5) {
-        // finalColor = vec4(random(time), random(time + 1.), random(time + 2.), 1.);
-        color = vec4(fragColor.r, 0., 0., 1.);
+        color = vec4(random(time), random(time + 1.), random(time + 2.), 1.);
+        // color = vec4(0., 0., 1., 1.);
     } else {
-        color = vec4(0., 0., 1., 1.0);
+        color = vec4(0., 0., 0., 1.);
     }
     gl_FragColor = color;
 }
