845c289531007b186a6ad78667acaa761974380b
dora-explora <sarrafdara@gmail.com>
2025-08-10 16:21:00 -0700

    random noise is per-pixel

--- assets/shader.fs
+++ assets/shader.fs
@@ -1,11 +1,14 @@
 #version 100
 precision mediump float;
 
-// varying vec2 fragTexCoord;
+varying vec2 fragTexCoord;
 varying vec4 fragColor;
 
-// uniform vec2 u_resolution;
 uniform float time;
+uniform int frameCount;
+
+const float resolutionX = 1920.;
+const float resolutionY = 1080.;
 
 vec4 color = vec4(0);
 
@@ -16,7 +19,12 @@ float random(in float seed) {
 void main()
 {
     if (fragColor.r > 0.5) {
-        color = vec4(random(time), random(time + 1.), random(time + 2.), 1.);
+        color = vec4(
+                random(time + fragTexCoord.x * resolutionX + fragTexCoord.y),
+                random(time + fragTexCoord.x * resolutionX + fragTexCoord.y + 1.),
+                random(time + fragTexCoord.x * resolutionX + fragTexCoord.y + 2.),
+                1.
+            );
         // color = vec4(0., 0., 1., 1.);
     } else {
         color = vec4(0., 0., 0., 1.);
