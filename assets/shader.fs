05d15e480c72a63053f903fa4aea9ec44aeaa86b
dora-explora <sarrafdara@gmail.com>
2025-08-10 14:49:35 -0700

    made it simpler because its not working :3

--- assets/shader.fs
+++ assets/shader.fs
@@ -1,21 +1,24 @@
-#version 330
+#version 100
+precision mediump float;
 
-in vec4 fragColor;
+// varying vec2 fragTexCoord;
+varying vec4 fragColor;
 
-uniform vec2 u_resolution;
-uniform float time;
+vec4 color = vec4(0);
+// uniform vec2 u_resolution;
+// uniform float time;
 
-out vec4 finalColor;
-
-float random(in float seed) {
-    return fract(sin(seed) * 43758.5453123);
-}
+// float random(in float seed) {
+//     return fract(sin(seed) * 43758.5453123);
+// }
 
 void main()
 {
     if (fragColor.r > 0.8) {
-        finalColor = vec4(random(time), random(time + 1.), random(time + 2.), 1.);
+        // finalColor = vec4(random(time), random(time + 1.), random(time + 2.), 1.);
+        color = vec4(1., 0., 0., 1.)
     } else {
-        finalColor = vec4(0.0, 1.0, 0.0, 1.0);
+        color = vec4(0., 0., 1., 1.0);
     }
+    gl_FragColor = color;
 }
