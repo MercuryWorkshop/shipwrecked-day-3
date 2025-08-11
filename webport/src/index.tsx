40dfe35ae2f09deca513c3f6395b5243e3558e2d
Toshit Chawda <r58playz@gmail.com>
2025-08-10 20:26:22 -0400

    play/pause

--- webport/src/index.tsx
+++ webport/src/index.tsx
@@ -1,4 +1,15 @@
-import { Component } from "dreamland/core";
+import { Component, css } from "dreamland/core";
+import { argbFromHex, DynamicScheme, Hct, Icon, SchemeStyles, ToggleButton, Variant } from "m3-dreamland";
+import playArrow from "@ktibow/iconset-material-symbols/play-arrow";
+import pause from "@ktibow/iconset-material-symbols/pause";
+
+let scheme = new DynamicScheme({
+	sourceColorHct: Hct.fromInt(argbFromHex("CBA6F7")),
+	contrastLevel: 0,
+	specVersion: "2025",
+	variant: Variant.TONAL_SPOT,
+	isDark: true,
+});
 
 let canvasElement = document.querySelector("canvas.emscripten");
 canvasElement.addEventListener("webglcontextlost", (e) => {
@@ -13,10 +24,34 @@ canvasElement.addEventListener("webglcontextlost", (e) => {
 	canvas: canvasElement,
 };
 
-const App: Component<{}> = function() {
+const App: Component<{}, { playing: boolean, }> = function() {
+	this.playing = true;
+
 	return (
 		<div id="app">
+			<SchemeStyles scheme={scheme} motion="expressive">
+				<div class="bottom">
+					<ToggleButton variant="elevated" icon="full" size="xl" value={use(this.playing)}>
+						<Icon icon={use(this.playing).andThen(pause, playArrow)} />
+					</ToggleButton>
+				</div>
+			</SchemeStyles>
 		</div>
 	)
 }
+App.style = css`
+	:scope :global(.m3dl-scheme-styles) {
+		font: var(--m3dl-font);
+		height: 100%;
+		display: flex;
+		flex-direction: column-reverse;
+	}
+
+	.bottom {
+		display: flex;
+		justify-content: center;
+		margin-bottom: 1rem;
+	}
+	.bottom > :global(.m3dl-button) { pointer-events: auto }
+`;
 document.querySelector("#app").replaceWith(<App />);
