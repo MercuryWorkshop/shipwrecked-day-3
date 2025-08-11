2cde2587e1174d88367fe1a14c0258282fdeb091
FoxMoss <foxmoss@mediaology.com>
2025-08-11 08:56:56 -0500

    fix pause play

--- sources/main.cpp
+++ sources/main.cpp
@@ -19,33 +19,33 @@ void set_enable_render_(bool enable) { enable_render = enable; }
 
 App app;
 void render_loop() {
-  app.time += GetFrameTime();
-  app.frame++;
   if (enable_render) {
-    SetShaderValue(app.shader, app.time_loc, &app.time, SHADER_UNIFORM_FLOAT);
-    SetShaderValue(app.shader, app.frame_loc, &app.frame, SHADER_UNIFORM_INT);
-
-    BeginTextureMode(app.target);
-    ClearBackground(BLACK);
-    BeginMode3D(app.camera);
-    DrawSphere({0, 1, 0}, 1, WHITE);
-    // DrawPlane({0, 0, 0}, {100, 100}, BLACK);
-    EndMode3D();
-    EndTextureMode();
-
-    BeginDrawing();
-    ClearBackground(WHITE);
-
-    BeginShaderMode(app.shader);
-    DrawTextureRec(app.target.texture,
-                   (Rectangle){0, 0, (float)app.target.texture.width,
-                               (float)-app.target.texture.height},
-                   (Vector2){0, 0}, WHITE);
-
-    EndShaderMode();
-
-    EndDrawing();
+    app.time += GetFrameTime();
+    app.frame++;
   }
+  SetShaderValue(app.shader, app.time_loc, &app.time, SHADER_UNIFORM_FLOAT);
+  SetShaderValue(app.shader, app.frame_loc, &app.frame, SHADER_UNIFORM_INT);
+
+  BeginTextureMode(app.target);
+  ClearBackground(BLACK);
+  BeginMode3D(app.camera);
+  DrawSphere({0, 1, 0}, 1, WHITE);
+  // DrawPlane({0, 0, 0}, {100, 100}, BLACK);
+  EndMode3D();
+  EndTextureMode();
+
+  BeginDrawing();
+  ClearBackground(WHITE);
+
+  BeginShaderMode(app.shader);
+  DrawTextureRec(app.target.texture,
+                 (Rectangle){0, 0, (float)app.target.texture.width,
+                             (float)-app.target.texture.height},
+                 (Vector2){0, 0}, WHITE);
+
+  EndShaderMode();
+
+  EndDrawing();
   return;
 }
 
