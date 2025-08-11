e9eae136f34e5c01c78306932385b4e52e8c8469
dora-explora <sarrafdara@gmail.com>
2025-08-10 18:45:18 -0700

    finished my part of the shader!!!!! (hopefully)

--- assets/shader.fs
+++ assets/shader.fs
@@ -9,27 +9,24 @@ uniform int frameCount;
 
 const float resolutionX = 1920.;
 const float resolutionY = 1080.;
-const int blinkRate = 2; // higher number = slower change
+const int blinkRate = 5; // higher number = higher change
 
-vec4 color = vec4(1., 0., 0., 1.);
+vec4 color = vec4(0., 0., 0., 1.);
 
 float random(in float seed) {
     return fract(sin(seed) * 43758.5453123);
 }
 
 vec4 noise(in float seed) {
-    return vec4(random(seed), random(seed + 1.), random(seed + 2.), 1.);
+    return vec4(random(seed), random(seed + 10.), random(seed + 20.), 1.);
 }
 
 void main()
 {
-    // if (fragColor.r > 0.5) {
-    if (true) {
-        color = noise(
-                float(frameCount / blinkRate) +
-                    fragTexCoord.x * resolutionX +
-                    (fragTexCoord.y + float(frameCount / blinkRate))
-            );
+    float seed = gl_FragCoord.x + fragTexCoord.y;
+    if (distance(gl_FragCoord.xy, vec2(400., 250.)) > 100.) {
+        seed += float(frameCount * blinkRate);
     }
+    color = noise(seed);
     gl_FragColor = color;
 }
